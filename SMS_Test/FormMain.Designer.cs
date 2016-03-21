35d388c17c34e402042b8f1e4593a3456927db02
MoreBlue <Payne.Zhang@usa.com>
2016-03-21 12:45:55 +0800

    20160321

---
+++ SMS_Test/FormMain.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace SMS_Test
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dtp = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnGetReport = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.checkBoxTiming = new System.Windows.Forms.CheckBox();
+            this.btnGetBalance = new System.Windows.Forms.Button();
+            this.checkBoxAddTimeSpan = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(12, 177);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(73, 11);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(280, 21);
+            this.txtNumber.TabIndex = 0;
+            this.txtNumber.Text = "13409000960";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(73, 35);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(280, 110);
+            this.txtContent.TabIndex = 1;
+            this.txtContent.Text = "您的SS账号邀请码/验证码为：【123】";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "收信号码";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "短信内容";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "发送时间";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(359, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "多个收信号码之间请用逗号隔开";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(359, 44);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 12);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "要求不超过500个字";
+            // 
+            // dtp
+            // 
+            this.dtp.Location = new System.Drawing.Point(73, 150);
+            this.dtp.Name = "dtp";
+            this.dtp.Size = new System.Drawing.Size(200, 21);
+            this.dtp.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(361, 156);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(125, 12);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "不打钩则默认立即发送";
+            // 
+            // btnGetReport
+            // 
+            this.btnGetReport.Location = new System.Drawing.Point(93, 177);
+            this.btnGetReport.Name = "btnGetReport";
+            this.btnGetReport.Size = new System.Drawing.Size(75, 23);
+            this.btnGetReport.TabIndex = 4;
+            this.btnGetReport.Text = "接收报告";
+            this.btnGetReport.UseVisualStyleBackColor = true;
+            this.btnGetReport.Click += new System.EventHandler(this.btnGetReport_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Location = new System.Drawing.Point(3, 206);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(546, 299);
+            this.txtResult.TabIndex = 5;
+            // 
+            // checkBoxTiming
+            // 
+            this.checkBoxTiming.AutoSize = true;
+            this.checkBoxTiming.Location = new System.Drawing.Point(280, 154);
+            this.checkBoxTiming.Name = "checkBoxTiming";
+            this.checkBoxTiming.Size = new System.Drawing.Size(72, 16);
+            this.checkBoxTiming.TabIndex = 11;
+            this.checkBoxTiming.Text = "定时发送";
+            this.checkBoxTiming.UseVisualStyleBackColor = true;
+            // 
+            // btnGetBalance
+            // 
+            this.btnGetBalance.Location = new System.Drawing.Point(174, 177);
+            this.btnGetBalance.Name = "btnGetBalance";
+            this.btnGetBalance.Size = new System.Drawing.Size(75, 23);
+            this.btnGetBalance.TabIndex = 12;
+            this.btnGetBalance.Text = "查询余额";
+            this.btnGetBalance.UseVisualStyleBackColor = true;
+            this.btnGetBalance.Click += new System.EventHandler(this.btnGetBalance_Click);
+            // 
+            // checkBoxAddTimeSpan
+            // 
+            this.checkBoxAddTimeSpan.AutoSize = true;
+            this.checkBoxAddTimeSpan.Location = new System.Drawing.Point(359, 129);
+            this.checkBoxAddTimeSpan.Name = "checkBoxAddTimeSpan";
+            this.checkBoxAddTimeSpan.Size = new System.Drawing.Size(96, 16);
+            this.checkBoxAddTimeSpan.TabIndex = 13;
+            this.checkBoxAddTimeSpan.Text = "是否追加时间";
+            this.checkBoxAddTimeSpan.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 509);
+            this.Controls.Add(this.checkBoxAddTimeSpan);
+            this.Controls.Add(this.btnGetBalance);
+            this.Controls.Add(this.checkBoxTiming);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnGetReport);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dtp);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.btnSend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormMain";
+            this.Text = "短信服务测试";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dtp;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnGetReport;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.CheckBox checkBoxTiming;
+        private System.Windows.Forms.Button btnGetBalance;
+        private System.Windows.Forms.CheckBox checkBoxAddTimeSpan;
+    }
+}
+
---
+++ SMS_Test/FormMain.cs
@@ -0,0 +1,183 @@
+﻿using SMS_Test.RemoteWebService;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMS_Test
+{
+    /// <summary>
+    /// 模块：发送短信
+    /// 作者：Anything
+    /// 日期：2016-03-21
+    /// 维护记录：
+    /// 说明：Winform测试短信服务Demo，后续将应用于调度任务定时读取数据库短信表新增数据，传给WebService接口，实现实时批量发送短信功能。
+    /// </summary>
+    public partial class FormMain : Form
+    {
+        #region 变量
+
+        private string _account = "***";//账号，此处已被隐藏，请询问山东电信客服获取
+        private string _pwd = "***";//密码，此处应填写md5加密后的密码
+        private string br = Environment.NewLine;//换行符 
+
+        #endregion
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 发送按钮
+        /// </summary>
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            mtMessage ms = new mtMessage();
+
+            /* 是否发送到多人 */
+            if (txtNumber.Text.Contains(','))
+            {
+                ms.phoneNumber = txtNumber.Text.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+            else
+            {
+                ms.phoneNumber = new string[] { txtNumber.Text };
+            }
+
+            /* 是否定时发送 */
+            if (checkBoxTiming.Checked)
+            {
+                ms.sendTime = dtp.Value;
+            }
+
+            ms.content = txtContent.Text;
+
+            /* 是否追加时间 */
+            if (checkBoxAddTimeSpan.Checked)
+            {
+                ms.content += DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff");
+            }
+
+            /* 执行发送 */
+            SmsOperatorClient soc = new SmsOperatorClient();
+            mtMessageRes res = soc.sendSms(_account, _pwd, "", ms);//todo:后续需从数据库读取数据，循环此方法实现批量发送。
+
+            /* 写记录 */
+            WriteSendLog(res);
+        }
+
+        /// <summary>
+        /// 接收报告
+        /// </summary>
+        private void btnGetReport_Click(object sender, EventArgs e)
+        {
+            /* 执行接收 */
+            SmsOperatorClient soc = new SmsOperatorClient();
+            reportMessageRes res = soc.getReport(_account, _pwd);
+
+            /* 写记录 */
+            WriteReportLog(res);
+        }
+
+        /// <summary>
+        /// 查询余额
+        /// </summary>
+        private void btnGetBalance_Click(object sender, EventArgs e)
+        {
+            /* 查询余额 */
+            SmsOperatorClient soc = new SmsOperatorClient();
+            balanceRes res = soc.getBalance(_account, _pwd);
+
+            /* 写记录 */
+            WriteBalanceLog(res);
+        }
+
+        /// <summary>
+        /// 登记发送记录
+        /// </summary>
+        /// <param name="res">传入参数</param>
+        private void WriteSendLog(mtMessageRes res)
+        {
+            string smsId = res.smsId;
+            string subStat = res.subStat;
+            string subStatDes = res.subStatDes;
+            List<string> ms = new List<string>();
+            if (res.resDetail == null)
+            {
+                ms.Add("\t--  res.SubStatDes == null");
+            }
+            else
+            {
+                foreach (mtMessageResDetail m in res.resDetail)
+                {
+                    ms.Add(string.Format("【phoneNumber】:{0}|【stat】:{1}|【statDes】:{2}|【ToString()】:{3}", m.phoneNumber, m.stat, m.statDes, m.ToString()));
+                }
+            }
+
+            txtResult.Text += "【发送】--------------【" + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff") + "】---------------" + br + "【smsId】:" + smsId + br + "【subStat】:" + subStat + br + "【subStatDes】:" + subStatDes + br;
+            foreach (string str in ms)
+            {
+                txtResult.AppendText(str + br);
+            }
+
+            /* 文本框光标滚动到底端 */
+            txtResult.ScrollToCaret();
+        }
+
+        /// <summary>
+        /// 登记接收报告
+        /// </summary>
+        /// <param name="res">传入参数</param>
+        private void WriteReportLog(reportMessageRes res)
+        {
+            string subStat = res.subStat;
+            string subStatDes = res.subStatDes;
+            List<string> rs = new List<string>();
+
+            if (res.resDetail == null)
+            {
+                rs.Add("\t--  res.resDetail == null");
+            }
+            else
+            {
+                foreach (reportMessageResDetail r in res.resDetail)
+                {
+                    rs.Add(string.Format("【smsId】:{0}|【phoneNumber】:{1}|【stat】:{2}|【statDes】:{3}|【ToString()】:{4}", r.smsId, r.phoneNumber, r.stat, r.statDes, r.ToString()));
+                }
+            }
+
+            txtResult.Text += "【接收报告】--------------【" + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff") + "】---------------" + br + "【subStat】:" + subStat + br + "【subStatDes】:" + subStatDes + br;
+            foreach (string str in rs)
+            {
+                txtResult.AppendText(str + br);
+            }
+
+            /* 文本框光标滚动到底端 */
+            txtResult.ScrollToCaret();
+        }
+
+        /// <summary>
+        /// 登记余额查询信息
+        /// </summary>
+        /// <param name="res">传入参数</param>
+        private void WriteBalanceLog(balanceRes res)
+        {
+            string revStat = res.revStat;
+            string revStatDes = res.revStatDes;
+            string demo = res.demo;
+            txtResult.AppendText("【余额】--------------【" + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff") + "】---------------" + br + "【revStat】:" + revStat + br + "【revStatDes】:" + revStatDes + br + "【demo】:" + demo + br);
+
+            /* 文本框光标滚动到底端 */
+            txtResult.ScrollToCaret();
+        }
+    }
+}
